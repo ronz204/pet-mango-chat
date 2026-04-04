8409e47c415fe63b631b58a7ed07977f6cc8d0b6
roney ruiz <roneyruizrojas@gmail.com>
2026-04-04 17:16:35 -0600

    feat(server): write update user profile query

--- server/source/core/dal/users/user.dao.ts
+++ server/source/core/dal/users/user.dao.ts
@@ -3,12 +3,17 @@ import { PrismaClient } from "@prisma/client";
 import { Search } from "./queries/search.query";
 import { Create } from "./queries/create.query";
 import { Obtain } from "./queries/obtain.query";
+import { Update } from "./queries/update.query";
 
 export class UserDao implements IUserDao {
   constructor(private prisma: PrismaClient) {};
 
   public async create(args: Create.Args) {
     return await this.prisma.user.create(Create.query(args));
+  };
+
+  public async update(args: Update.Args) {
+    return await this.prisma.user.update(Update.query(args));
   };
 
   public async search(args: Search.Args) {
