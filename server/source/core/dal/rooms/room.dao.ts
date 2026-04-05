0354c5d7ce2d245377b1a50e322c7c505084b640
roney ruiz <roneyruizrojas@gmail.com>
2026-04-05 16:25:58 -0600

    feat(server): create leave room query

--- server/source/core/dal/rooms/room.dao.ts
+++ server/source/core/dal/rooms/room.dao.ts
@@ -1,11 +1,17 @@
 import type { IRoomDao } from "./room.idao";
 import { PrismaClient } from "@prisma/client";
+
+import { Leave } from "./queries/leave.query";
 import { Create } from "./queries/create.query";
 import { Obtain } from "./queries/obtain.query";
 import { Invitees } from "./queries/invitees.query";
 
 export class RoomDao implements IRoomDao {
   constructor(private prisma: PrismaClient) {};
+
+  public async leave(args: Leave.Args) {
+    return await this.prisma.member.delete(Leave.query(args));
+  };
 
   public async create(args: Create.Args) {
     return await this.prisma.room.create(Create.query(args));
