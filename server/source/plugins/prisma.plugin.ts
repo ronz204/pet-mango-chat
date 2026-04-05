31cc9012bfd8590e1b2fb5423e26b8db0fa8dc9e
roney ruiz <roneyruizrojas@gmail.com>
2026-04-05 16:37:27 -0600

    chore(server): format prisma plugin

--- server/source/plugins/prisma.plugin.ts
+++ server/source/plugins/prisma.plugin.ts
@@ -1,9 +1,10 @@
+import { Elysia } from "elysia";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Elysia } from "elysia";
 
+const name: string = "prisma.plugin";
 const url = process.env.POSTGRES_URL ?? "";
 const adapter = new PrismaPg({ connectionString: url });
 
-export const PrismaPlugin = new Elysia({ name: "prisma" })
+export const PrismaPlugin = new Elysia({ name })
   .decorate("prisma", new PrismaClient({ adapter }));
