79e769826216f1355d59a82f5c0a20be08605ab8
roney ruiz <roneyruizrojas@gmail.com>
2026-04-06 23:02:52 -0600

    deps(client): install and config vue query

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,13 @@
 import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+
 import { Router } from "./router";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
 const app = createApp(App);
+
 app.use(Router);
+app.use(VueQueryPlugin);
+
 app.mount("#app");
