30d0551c3990dbe9623c4f3a37811e8b3eae6aee
roney ruiz <roneyruizrojas@gmail.com>
2026-04-06 23:00:15 -0600

    deps(client): install and config vue router

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+import { Router } from "./router";
 
 const app = createApp(App);
+app.use(Router);
 app.mount("#app");
---
+++ client/src/router.ts
@@ -0,0 +1,6 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+export const Router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
