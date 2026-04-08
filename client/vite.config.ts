6775fa15435ced821ab01cb399c2ff5d27c7394d
roney ruiz <roneyruizrojas@gmail.com>
2026-04-07 21:48:28 -0600

    feat(client): add tspaths alias to vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: { alias: {
+    "@features": "/src/features",
+    "@components": "/src/components",
+  }},
 });
