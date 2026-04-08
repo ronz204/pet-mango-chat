ba477299845a6c28c52f524b73421ef24ee8692a
roney ruiz <roneyruizrojas@gmail.com>
2026-04-07 21:58:24 -0600

    deps(client): install and config pinia stores

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,15 @@
 import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 import { Router } from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(Router);
 app.use(VueQueryPlugin);
 
