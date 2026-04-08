5b26579adde50cd8f77e99c6851095a11c8f21f9
roney ruiz <roneyruizrojas@gmail.com>
2026-04-08 12:28:10 -0600

    chore(client): remove all ai generated components

--- client/src/playground/identity/containers/LoginContainer.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import LoginForm from '../components/LoginForm.vue';
-</script>
-
-<template>
-  <div>
-    <LoginForm />
-  </div>
-</template>
--- client/src/router.ts
+++ client/src/router.ts
@@ -1,12 +1,6 @@
-import { AuthGuard } from "@guards/auth.guard";
-import { IdentityRoutes } from "@features/identity/routes";
 import { createWebHistory, createRouter } from "vue-router";
 
 export const Router = createRouter({
   history: createWebHistory(),
-  routes: [
-    ...IdentityRoutes,
-  ],
+  routes: [],
 });
-
-Router.beforeEach(AuthGuard);
