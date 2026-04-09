648ef1de57e665fddc161af717ce9e72b305d898
roney ruiz <roneyruizrojas@gmail.com>
2026-04-08 23:17:20 -0600

    feat(client): create identity provider types

--- client/src/providers/identity/identity.provider.ts
+++ client/src/providers/identity/identity.provider.ts
@@ -1,5 +1,16 @@
 import { BaseProvider } from "@providers/base.provider";
 
-export class IdentityProvider extends BaseProvider {
+import type {
+  SignInRequest,
+  SignUpRequest,
+} from "./identity.types";
 
+export class IdentityProvider extends BaseProvider {
+  public async signIn(req: SignInRequest) {
+    console.log(req);
+  };
+
+  public async signUp(req: SignUpRequest) {
+    console.log(req);
+  };
 };
