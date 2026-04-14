9ec5079f1d683215d8e2f986a531bc42150563b7
roney ruiz <roneyruizrojas@gmail.com>
2026-04-13 21:06:12 -0600

    fix(server): add patch to cors plugin

--- server/source/plugins/cors.plugin.ts
+++ server/source/plugins/cors.plugin.ts
@@ -2,6 +2,6 @@ import cors from "@elysiajs/cors";
 
 export const CorsPlugin = cors({
   origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 });
