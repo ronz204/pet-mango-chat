471b17670e28cb563401a6a6ebcd5a091292aec2
roney ruiz <roneyruizrojas@gmail.com>
2026-04-21 09:57:05 -0600

    chore(server): fix plugins imports

--- server/source/features/profile/profile.plugin.ts
+++ server/source/features/profile/profile.plugin.ts
@@ -1,10 +1,10 @@
 import { Elysia } from "elysia";
-import { GetProfilePlugin } from "./get-profile/get-profile.plugin";
-import { UpdateInfoPlugin } from "./update-info/update-info.plugin";
+import { GetProfileInfoPlugin } from "./get-profile-info/get-profile-info.plugin";
+import { UpdateBasicInfoPlugin } from "./update-basic-info/update-basic-info.plugin";
 
 const prefix: string = "/profile";
 const name: string = "profile.plugin";
 
 export const ProfilePlugin = new Elysia({ name, prefix })
-  .use(GetProfilePlugin).use(UpdateInfoPlugin);
-
+  .use(GetProfileInfoPlugin)
+  .use(UpdateBasicInfoPlugin);
